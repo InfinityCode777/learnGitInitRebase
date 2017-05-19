40a7ec001b97fc02137298381b5893f9644eac26
Jing Wang <jing@figur8.me>
2017-05-19 17:19:41 -0400

    Init commit

---
+++ learnGitInitRebase/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  learnGitInitRebase
+//
+//  Created by Jing Wang on 5/19/17.
+//  Copyright © 2017 figur8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var showQuestionBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
